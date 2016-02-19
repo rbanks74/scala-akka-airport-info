ede4e5bee09276a8854eca97b10e28d1b03f9f21
rbrutgers <rbanks@viridityenergy.com>
2016-02-18 20:09:46 -0500

    Creating template, testing functionality of worker actor in main.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,27 +1,38 @@
-import play.api.libs.json.{Json, JsValue}
+import java.util.Calendar
+
+import play.api.libs.json.Json._
+import play.api.libs.json.{JsObject, JsString, Json}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
 import scala.io.Source
 
 
-
 object Main extends App {
 
-  //println(Source.fromURL("http://services.faa.gov/airport/status/PHL?format=application/json").mkString)
-
   val iata = List("PHL","RDU", "BWI", "JAX", "BOS", "DAL")
-
   val url = "http://services.faa.gov/airport/status/PHL?format=application/json"
 
   def downloadPage(url: String): Future[String] = Future(Source.fromURL(url).mkString)
 
-  downloadPage(url).foreach(println)
-  //Thread.sleep(2000)
+  def toJsonAndTransform(data: String): JsObject = {
+    val pageData = parse(data)
+    val currentTimeStamp = Calendar.getInstance().getTime
 
-  val json: JsValue = Json.parse(Source.fromURL("http://services.faa.gov/airport/status/PHL?format=application/json").mkString)
+    val cleansedData = JsObject(Seq(
+      "iataCode" -> (pageData \ "IATA").get,
+      "state" -> (pageData \ "state").get,
+      "airportName" -> (pageData \ "name").get,
+      "status" -> (pageData \ "status").get,
+      "time" -> JsString(currentTimeStamp.toString)
+    ))
 
-  println(Json.prettyPrint(json))
+    println(Json.prettyPrint(cleansedData))
+    cleansedData
+  }
+
+  val json = downloadPage(url).map(urlContent => toJsonAndTransform(urlContent))
+  Thread.sleep(2000)
 
 
 }
